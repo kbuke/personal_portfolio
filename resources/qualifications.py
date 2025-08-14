c8a20b49a711bdd54e9cccfc431e3c538d6f3c8b
Kaan Buke <kabuke13@gmail.com>
2025-08-14 14:20:01 +0200

    allows user to alter qualification information

--- resources/qualifications.py
+++ resources/qualifications.py
@@ -3,7 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from schemas import QualificationSchema
+from schemas import QualificationSchema, QualificationUpdateSchema
 
 from models import QualificationModel
 
@@ -18,6 +18,22 @@ class Qualification(MethodView):
     @blp.response(200, QualificationSchema)
     def get(self, qualification_id):
         qualification = QualificationModel.query.get_or_404(qualification_id)
+        return qualification
+
+    @blp.arguments(QualificationUpdateSchema)
+    @blp.response(200, QualificationSchema)
+    def put(self, qualification_data, qualification_id):
+        qualification = QualificationModel.query.get(qualification_id)
+        if qualification:
+            qualification.qualification_title = qualification_data["qualification_title"]
+            qualification.qualifciation_img = qualification_data["qualifciation_img"]
+            qualification.qualification_date = qualification_data["qualification_date"]
+            qualification.about_id = qualification_data["about_id"]
+            qualification.institute_id = qualification_data["institute_id"]
+        else:
+            qualification = QualificationModel(id=qualification_id, **qualification_data)
+        db.session.add(qualification)
+        db.session.commit()
         return qualification
 
 @blp.route("/qualification")
